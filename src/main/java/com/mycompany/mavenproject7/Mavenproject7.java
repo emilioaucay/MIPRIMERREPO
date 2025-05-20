1fcb62d5fcc4313a7dbf0b97fbe3918bfc001450
ESTUDIANTE <ESTUDIANTE@DESKTOP-VT9DPKT>
2025-05-20 13:53:21 -0500

    MI PRIMER RESPOSITORO EN APACHE NETBEANS Y JAVA

--- src/main/java/com/mycompany/mavenproject7/Mavenproject7.java
+++ src/main/java/com/mycompany/mavenproject7/Mavenproject7.java
@@ -4,13 +4,18 @@
 
 package com.mycompany.mavenproject7;
 
-/**
- *
- * @author ESTUDIANTE
- */
+import java.util.Scanner;
+
+
 public class Mavenproject7 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int suma = n1 + n2;
+        System.out.println("la suma de los digitos es" +suma);
     }
 }
+
+    
